4a0c592a282e0f812f21ea832e01010f67309623
JessyEngwall <145036227+JessyEngwall@users.noreply.github.com>
2023-09-21 09:27:58 -0600

    Create cod.olimpiadas1.py

---
+++ cod.olimpiadas1.py
@@ -0,0 +1,53 @@
+def area():
+  x = (b*a)/2
+  return x
+
+a = float(input("DAME LA ALTURA DE TU TRIÁNGULO: "))
+b = float(input("DAME LA BASE DE TU TRIÁNGULO: "))
+
+print("EL ÁREA DE TU TRIÁNGULO ES: ", area())
+
+
+def suma(a,b):
+  x = a + b
+  return x
+
+def resta(a, b):
+  x = a - b
+  return x
+
+print( "Dame el primer número:")
+a = int( input())
+
+print( "Dame el segundo número:")
+b = int( input ( ) )
+
+
+def conversion(kilometros):
+  metros = kilometros * 1000
+  return metros
+
+# Pedir al usuario la cantidad de kilómetros recorridos kilometros - float( input ("Por favor, ingresa la cantidad de kilómetros recorridos:
+" ))
+
+# Llamar a la función para realizar la conversión
+resultado_en_metros = conversion(kilometros)
+
+# Mostrar el resultado al usuario
+
+print(f*(kilometros}kilómetros equivalen a (resultado_en_metros)
+metros."
+
+
+
+def conversion( kilometros):
+  metros = kilometros * 1000
+  return metros
+
+# Pedir al usuario la cantidad de kilómetros recorridos kilometros - float( input("Por favor, ingresa la cantidad de kilómetros
+recorridos: "))
+
+# Llamar a la función para realizar la conversión
+resultado_en_metros = conversion(kilometros)
+# Mostrar el resultado al usuario
+print(f"(kilometros} kilómetros equivalen a {resultado_en _metros} metros.")
